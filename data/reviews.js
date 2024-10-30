a652d745696b51783f5f69919b2d4f7796198517
Tayleigh Ward <tayleigh.ward24@gmail.com>
2024-10-30 01:30:49 -0600

    trying agian

---
+++ data/reviews.js
@@ -0,0 +1,27 @@
+export const reviews = [
+    {
+        "name": "Ethan R.",
+        "stars": 4,
+        "text": "Top-notch service! I brought my car in for a performance upgrade, and the team at Albert's delivered beyond my expectations. My car feels like new! Highly recommended!"
+    },
+    {
+        "name": "Lana S.",
+        "stars": 5,
+        "text": "Best detailing service in town. My car's ceramic coating looks incredible, and the interior is spotless. The team really knows luxury cars inside and out!"
+    },
+    {
+        "name": "James T.",
+        "stars": 3,
+        "text": "Exceptional concierge service. They picked up my car for maintenance, provided a sleek loaner, and returned mine fully serviced. Such a smooth experience!"
+    },
+    {
+        "name": "Michelle D.",
+        "stars": 4,
+        "text": "Trustworthy and professional. I’ve been bringing my car here for regular maintenance, and Albert’s always does a fantastic job. My car runs beautifully!"
+    },
+    {
+        "name": "Brad P.",
+        "stars": 5,
+        "text": "Worth every penny! I’m particular about my car’s detailing, and Albert’s exceeded my standards. They treat every car like it’s a luxury."
+    }
+];
---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+import {reviews} from "../data/reviews.js"
+
+const navElement = document.querySelector('#theNav')
+const btnElement = document.querySelector('#myButton')
+
+btnElement.addEventListener('click', () => {
+    btnElement.classList.toggle('open')
+    navElement.classList.toggle('open')
+
+})
+
+//grab the reviews
+
+console.log(reviews)
+
+//create and empty card
+const myTarget = document.querySelector("#cards")
+
+for (let x = 0; x < reviews.length; x++) {
+
+    //create a section
+    const mySection = document.createElement('section')
+    const myName = document.createElement('h3')
+    myName.textContent = reviews[x].name
+
+    const myReview = document.createElement('p')
+    myReview.textContent = reviews[x].text
+
+    const myRating = document.createElement('div')
+
+    for (let step = 0; step<reviews[x].stars; step++) {
+        const myStar = document.createElement('img')
+        myStar.src = "../images/star-svg.svg"
+        myRating.appendChild(myStar)
+    }
+       
+
+    //build the figure with child elements
+    mySection.appendChild(myName)
+    mySection.appendChild(myRating)
+    mySection.appendChild(myReview)
+
+    //add a new card to a page
+    myTarget.appendChild(mySection)
+
+
+}
+
+
+// inject right year
+const rightNow = new Date()
+document.querySelector('#year').textContent = rightNow.getFullYear()
